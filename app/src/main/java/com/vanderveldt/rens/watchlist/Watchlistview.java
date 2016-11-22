79284b0edf49e779255b85cdb0f0c5aeefe7d4db
bakenbraad <rensvanderveldt@gmail.com>
2016-11-22 13:00:29 +0100

    custom adapter for hashmaps added

--- app/src/main/java/com/vanderveldt/rens/watchlist/Watchlistview.java
+++ app/src/main/java/com/vanderveldt/rens/watchlist/Watchlistview.java
@@ -8,17 +8,20 @@ import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +34,8 @@ public class Watchlistview extends AppCompatActivity {
     public static final String MyPREFERENCES = "myprefs";
     SharedPreferences prefs;
     Context context;
+    Map<String, ?> titles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +47,19 @@ public class Watchlistview extends AppCompatActivity {
         final ListView lv = (ListView) findViewById(R.id.listView);
 
         // Get content for listview in titles form. (WOKRS)
-        prefs = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        String[] titleStrings = new String[]{"Batman", "Superman", "Pirates of the Caribbean", "Fury", "Harry Potter and the chamber of secrets"};
+        //Map<String, String> titlestrings = new HashMap<String, String>();
 
+        //prefs = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        //titles = prefs.getAll();
+
+        //for (Map.Entry<String, ?> entry : titles.entrySet()) {
+        //    titlestrings.put(entry.getKey().toString(),entry.getValue().toString());
+        //}
 
         // Set the adapter (WOKRS)
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titleStrings);
+        //MyAdapter adapter = new MyAdapter(titlestrings);
+        String[] titles = {"Batman", "Superman", "Iron man", "Pirates of the Caribbean", "Harry Potter and the Chamber of secrets"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
         lv.setAdapter(adapter);
 
         // Set listener (WOKRS)
